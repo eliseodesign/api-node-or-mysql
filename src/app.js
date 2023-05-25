417b1992995076fdab8d7e1c5f209e855f2e8f21
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-05-24 20:29:23 -0600

    [:sparkles: add] conection mongo atlas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
+require("dotenv").config()
 const express = require("express")
+const dbConnect = require("./config/mongo")
 const cors = require("cors")
 
 const app = express()
 app.use(cors())
 
-const PORT = 3021
+const PORT = process.env.PORT || 3021
 
 app.listen(PORT, ()=>{
   console.log(`🚀 the app is running on http://localhost:${PORT}`)
 })
+
+dbConnect()
