fd51497251cee0233fb77c33f9f6d9561d16ecf6
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-05-26 10:28:11 -0600

    [:sparkles: add] Models

---
+++ src/models/nosql/tracks.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose")
+
+
+const TracksSchema = new mongoose.Schema(
+  {
+    name:{
+      type:String
+    },
+    album:{
+      type:String
+    },
+    cover:{
+      type:String,
+      validator:(req)=>{
+        return true
+      },
+      message:"ERROR_URL"
+    },
+    filename:{
+      type:Number
+    },
+    artist:{
+      name:{
+        type:String
+      },
+      nickname:{
+        type:String
+      },
+      nationality:{
+        type:String
+      }
+    },
+    duration:{
+      start:{
+        type:Number
+      },
+      end:{
+        type:Number
+      }
+    },
+    mediaId:{
+      type: mongoose.Types.ObjectId
+    }
+  },
+  {
+    timestamps:true,
+    versionKey:false
+  }
+)
+
+module.exports = mongoose.model("tracks", TracksSchema)
